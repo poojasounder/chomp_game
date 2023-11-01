5aa7308c35e51aed5a02621c1c4f7d9b896a2aa0
Pooja <pooja3@pdx.edu>
2023-11-01 16:05:38 -0700

    reading in user input for width and height and converting them to usize

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 extern crate chomp_game;
+extern crate prompted;
 
+use prompted::input;
 use chomp_game::Board;
+
 fn main() {
-    let board = Board::create_board(4,5);
+
+    let user_width = input!("Enter a width for the board: ");
+    let user_height = input!("Enter a height for the board: ");
+    
+     // Convert the user input to usize
+    let width: usize = user_width.trim().parse().unwrap();
+    let height: usize = user_height.trim().parse().unwrap();
+
+    let board = Board::create_board(width,height);
     Board::print_board(&board)
 }
