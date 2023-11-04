6bbea2e181f190938ab5ba62aafb63d14bf22d6a
Pooja <pooja3@pdx.edu>
2023-11-04 00:14:49 -0700

    Added comments to all the functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,14 @@ pub struct Board {
 }
 
 impl Board {
+    // function to create a board with a given width and height
     pub fn create_board(width: usize, height: usize) -> Self {
         assert!(
             width <= MAX_WIDTH && height <= MAX_HEIGHT,
             "Width must be lesser than or equal to 4 and Height must be lesser than or equal to 5"
         );
+
+        // setting all the squares to true initially
         let squares = [[true; MAX_WIDTH]; MAX_HEIGHT];
         Board {
             width,
@@ -22,6 +25,7 @@ impl Board {
         }
     }
 
+    // function to dispplay the board
     pub fn print_board(&self) {
         for row in 0..self.height {
             for col in 0..self.width {
@@ -32,6 +36,8 @@ impl Board {
         }
     }
 
+    // Chomp a given square, removing all squares below it
+    // and to the right of it
     pub fn chomp(&mut self, row: usize, col: usize) {
         for r in row..self.height {
             for c in col..self.width {
@@ -41,14 +47,27 @@ impl Board {
     }
 
     pub fn winning_move(&self) -> Option<(usize, usize)> {
+        // Check whether the board state is already lost.
+        //If so, then there is no winning move
+        if self.is_game_over() == true {
+            return None;
+        }
+
+        // Otherwise for each possible move
         for r in 0..self.height {
             for c in 0..self.width {
                 if r == 0 && c == 0 {
                     continue;
                 }
                 if self.squares[r][c] {
+                    // Create a new_board
                     let mut new_board = self.clone();
+                    //Perform the move on new_board
                     new_board.chomp(r, c);
+                    //Call winning_move recursively at new_board and
+                    //if winning_move outputs a winning_move for new_board
+                    //then this move is not a winning_move . Continue to the next move
+                    //Otherwise, this is the winning move and return it
                     if new_board.winning_move() == None {
                         return Some((r, c));
                     }
@@ -57,15 +76,23 @@ impl Board {
         }
         return None;
     }
+
+    // Function to check if the game is over.
+    // the board state is lost if the upper-left square is the only one left
     pub fn is_game_over(&self) -> bool {
+        // total number of squares in the board
         let mut count = self.width * self.height;
         for i in 0..self.height {
             for j in 0..self.width {
+                // if the square is labeled as false ---> eaten
                 if !self.squares[i][j] {
                     count -= 1;
                 }
             }
         }
+
+        // if there is only one square left and it is the upper-left square,
+        // return true else, return false
         if count == 1 && self.squares[0][0] {
             return true;
         }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use prompted::input;
 fn main() {
     println!("Welcome to my chomp game");
     println!("Let's create your custom board!");
+
+    // read user input for their desired board size
     let user_width = input!("Enter a width for the board: ");
     let user_height = input!("Enter a height for the board: ");
 
@@ -14,9 +16,15 @@ fn main() {
     let width: usize = user_width.trim().parse().unwrap();
     let height: usize = user_height.trim().parse().unwrap();
 
+    // Create the board of the desired width and height
     let mut board = Board::create_board(width, height);
+
+    // display the board
     Board::print_board(&board);
+
+    // Run this loop till game is over
     while Board::is_game_over(&board) == false {
+        // Ask the user which row and column they would want to chomp
         let user_move_row: usize = input!("Enter a row for the square you want to remove: ")
             .trim()
             .parse()
@@ -26,8 +34,10 @@ fn main() {
             .parse()
             .unwrap();
 
-        Board::chomp(&mut board, user_move_row - 1, user_move_col - 1);
+        Board::chomp(&mut board, user_move_row, user_move_col);
         Board::print_board(&board);
+
+        // Try to find a winning move. If there is one, chomp the winning_move
         if let Some(winning_move) = Board::winning_move(&board) {
             println!(
                 "The winning move is: ({}, {})",
@@ -35,6 +45,10 @@ fn main() {
             );
             Board::chomp(&mut board, winning_move.0, winning_move.1);
         };
+
+        // Otherwise, stall by chomping as little as possible
+
+        // display the board again after the AI played
         Board::print_board(&board);
     }
 }
