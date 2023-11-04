166fbd1ebb9f8ae0c132f570237ee2fc2877142b
Pooja <pooja3@pdx.edu>
2023-11-03 23:10:10 -0700

    Completed winning _move function

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,74 @@
-
 const MAX_WIDTH: usize = 5;
 const MAX_HEIGHT: usize = 4;
 
 #[derive(Clone)]
-pub struct Board{
+pub struct Board {
     width: usize,
     height: usize,
     squares: [[bool; MAX_WIDTH]; MAX_HEIGHT],
 }
 
-impl Board{
-
-    pub fn create_board(width: usize, height:usize) -> Self{
-        assert!(width<=MAX_WIDTH && height <=MAX_HEIGHT, "Width must be lesser than or equal to 4 and Height must be lesser than or equal to 5");
-        let squares = [[true;MAX_WIDTH];MAX_HEIGHT];
-        Board{
+impl Board {
+    pub fn create_board(width: usize, height: usize) -> Self {
+        assert!(
+            width <= MAX_WIDTH && height <= MAX_HEIGHT,
+            "Width must be lesser than or equal to 4 and Height must be lesser than or equal to 5"
+        );
+        let squares = [[true; MAX_WIDTH]; MAX_HEIGHT];
+        Board {
             width,
             height,
             squares,
         }
     }
 
-    pub fn print_board(&self){
-        
+    pub fn print_board(&self) {
         for row in 0..self.height {
             for col in 0..self.width {
-                let symbol = if self.squares[row][col] {
-                    'X'
-                } else {
-                    ' '
-                };
+                let symbol = if self.squares[row][col] { 'X' } else { ' ' };
                 print!("{}", symbol);
             }
             println!();
         }
     }
-    pub fn chomp(&mut self,row:usize, col: usize){
-        for r in row..self.height{
-            for c in col..self.width{
+
+    pub fn chomp(&mut self, row: usize, col: usize) {
+        for r in row..self.height {
+            for c in col..self.width {
                 self.squares[r][c] = false;
             }
         }
     }
 
-    
-    pub fn winning_move(&self) -> Option<(usize, usize)>{
-        for r in 0..self.height{
-            for c in 0..self.width{
-                if r == 0 && c==0{
+    pub fn winning_move(&self) -> Option<(usize, usize)> {
+        for r in 0..self.height {
+            for c in 0..self.width {
+                if r == 0 && c == 0 {
                     continue;
                 }
-                let mut new_board = self.clone();
-                new_board.chomp(r,c);
-                if new_board.squares[0][0]{
-                    return Some((r,c));
+                if self.squares[r][c] {
+                    let mut new_board = self.clone();
+                    new_board.chomp(r, c);
+                    if new_board.winning_move() == None {
+                        return Some((r, c));
+                    }
                 }
             }
         }
-        return None
+        return None;
     }
-
-    pub fn is_game_over(&self) -> bool{
+    pub fn is_game_over(&self) -> bool {
         let mut count = self.width * self.height;
-        for i in 0..self.height{
-            for j in 0..self.width{
-                if !self.squares[i][j]{
+        for i in 0..self.height {
+            for j in 0..self.width {
+                if !self.squares[i][j] {
                     count -= 1;
                 }
-
             }
         }
-        
-        if count == 1 && self.squares[0][0]{
+        if count == 1 && self.squares[0][0] {
             return true;
         }
         return false;
     }
-
 }
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 extern crate chomp_game;
 extern crate prompted;
 
-use prompted::input;
 use chomp_game::Board;
+use prompted::input;
 
-
-fn main(){
-
+fn main() {
     println!("Welcome to my chomp game");
     println!("Let's create your custom board!");
     let user_width = input!("Enter a width for the board: ");
     let user_height = input!("Enter a height for the board: ");
 
-     // Convert the user input to usize
+    // Convert the user input to usize
     let width: usize = user_width.trim().parse().unwrap();
     let height: usize = user_height.trim().parse().unwrap();
 
-    let mut board = Board::create_board(width,height);
+    let mut board = Board::create_board(width, height);
     Board::print_board(&board);
-    while Board::is_game_over(&board) == false{
-        print!("{}",Board::is_game_over(&board));
-        let user_move_row: usize = input!("Enter a row for the square you want to remove: ").trim().parse().unwrap();
-        let user_move_col: usize = input!("Enter a col for the square you want to remove: ").trim().parse().unwrap();
-    
-        Board::chomp(&mut board,user_move_row-1,user_move_col-1);
+    while Board::is_game_over(&board) == false {
+        let user_move_row: usize = input!("Enter a row for the square you want to remove: ")
+            .trim()
+            .parse()
+            .unwrap();
+        let user_move_col: usize = input!("Enter a col for the square you want to remove: ")
+            .trim()
+            .parse()
+            .unwrap();
+
+        Board::chomp(&mut board, user_move_row - 1, user_move_col - 1);
         Board::print_board(&board);
-        Board::winning_move(&board);
-        //Board::print_board(&board);
+        if let Some(winning_move) = Board::winning_move(&board) {
+            println!(
+                "The winning move is: ({}, {})",
+                winning_move.0, winning_move.1
+            );
+            Board::chomp(&mut board, winning_move.0, winning_move.1);
+        };
+        Board::print_board(&board);
     }
-    
 }
